28e61303c7fee9e61d90a1248a082476ccbd68e4
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-06 22:50:40 +0800

    add_library(${ProjectName} OBJECT)

--- project.cmake
+++ project.cmake
@@ -1,8 +1,3 @@
-set(ProjectName FatFs)
-project(${ProjectName})
-enable_language(ASM)
-set(CMAKE_CXX_STANDARD 20)
-set(CMAKE_C_STANDARD 99)
-
-add_library(${ProjectName} STATIC)
+add_library(${ProjectName} OBJECT)
 target_import_src(${ProjectName})
+target_install_obj_dir(${ProjectName})
